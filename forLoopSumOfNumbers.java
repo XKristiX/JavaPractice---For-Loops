cd13a44ca2dd3069b88559c562d3b4bef23a88fe
Kristi Smith <whippedcreem@gmail.com>
2023-07-15 08:58:10 -0400

    for loops examples

---
+++ forLoopSumOfNumbers.java
@@ -0,0 +1,11 @@
+public class forLoopSumOfNumbers {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            sum += i;
+        }
+
+        System.out.println("The sum of the numbers from 1 to 100 is: " + sum);
+    }
+}
---
+++ forloopOddNumber.java
@@ -0,0 +1,8 @@
+public class forloopOddNumber {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 15; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
+
